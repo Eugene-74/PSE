ece225a43b47144b728daab9d48ca7a0e5d79383
Eugene-74 <eugenearg@gmail.com>
2025-03-04 16:37:08 +0100

    update

--- simulation.cpp
+++ simulation.cpp
@@ -1,13 +1,14 @@
-// g++ main.cpp -o game -lglfw -lGL -ldl -lX11 -pthread
+// g++ -o mon_projet.exe simulation.cpp glad.c -Iinclude -Llib -lglfw3 -lopengl32 -lgdi32
 // ./simulation.cpp
 
+#include "glad/include/glad/glad.h"
 #include <GLFW/glfw3.h>
-#include <glad/glad.h>
 
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 #include <iostream>
+#include <vector>
 
 // Shader source
 const char* vertexShaderSource = R"(
@@ -30,40 +31,86 @@ void main()
 }
 )";
 
+
+
 const char* fragmentShaderSource = R"(
-#version 330 core
-out vec4 FragColor;
+    #version 330 core
+    out vec4 FragColor;
+    
+    in vec3 FragPos;
+    in vec3 Normal;
+    
+    uniform vec3 lightPos;
+    uniform vec3 viewPos;
+    uniform bool fastSqrt;
+    
+    float simpleSqrt(float nbr) {
+    float epsilon = 0.0001;
+        float racine = 0;
+        int iteration = 0;
+        int i, j;
+        for (j = 0; j < epsilon; j++) {
+            i = 0;
+            while ((racine + i * epsilon) * (racine + i * epsilon) < nbr) {
+                iteration += 1;
+                i += 1;
+            }
+            i -= 1;
+            racine += i * epsilon;
+        }
+        return 1/racine;
+    }
 
-in vec3 FragPos;
-in vec3 Normal;
+    // Approximation de la racine carrée inverse
+    float fastInvSqrt(float x) {
+        int i = floatBitsToInt(x);
+        i = 0x5F3759DF - (i >> 1);
+        float y = intBitsToFloat(i);
+        return y * (1.5 - 0.5 * x * y * y);
+    }
+    
+    float sqrtHerron(float number) {
+        float x = number;
+        
+        for (int i = 0; i < 10; i++) {
+            x = (x + number / x) / 2;
+        }
 
-uniform vec3 lightPos;
-uniform vec3 viewPos;
-
-// Approximation de la racine carrée inverse
-float fastInvSqrt(float x) {
-    int i = floatBitsToInt(x);
-    i = 0x5F3759DF - (i >> 1);
-    float y = intBitsToFloat(i);
-    return y * (1.5 - 0.5 * x * y * y);
+    return 1 / x;
 }
 
-void main()
-{
-    vec3 norm = Normal;
-    vec3 lightDir = normalize(lightPos - FragPos);
-    
-    // Normalisation avec l'inverse sqrt rapide
-    float lengthSq = dot(lightDir, lightDir);
-    float invLength = fastInvSqrt(lengthSq);
-    lightDir *= invLength;
+    void main()
+    {
+       vec3 norm = normalize(Normal);
+        vec3 lightDir = lightPos - FragPos;
+        
+        float lengthSq = dot(lightDir, lightDir);
+       
+        float invLength;
+        if (fastSqrt) {
+            invLength = fastInvSqrt(lengthSq);
+        } else {
+            invLength = sqrtHerron(lengthSq);
+        }
+        lightDir *= invLength;
 
-    float diff = max(dot(norm, lightDir), 0.0);
-    vec3 diffuse = diff * vec3(1.0, 1.0, 1.0); // Lumière blanche
-    
-    FragColor = vec4(diffuse, 1.0);
-}
-)";
+        float distance = length(lightPos - FragPos);
+        float attenuation = 1.0 / (1.0 + 0.1 * distance + 0.5 * (distance * distance));
+
+        // Diffuse component for lightDir
+        float diff1 = max(dot(norm, lightDir), 0.0);
+        vec3 diffuse1 = attenuation * diff1 * vec3(1.0, 1.0, 1.0); // White light
+
+        // Diffuse component for -lightDir
+        float diff2 = max(dot(norm, -lightDir), 0.0);
+        vec3 diffuse2 = attenuation * diff2 * vec3(1.0, 1.0, 1.0); // White light
+
+        // Combine both diffuse components
+        vec3 diffuse = diffuse1 + diffuse2;
+
+        FragColor = vec4(diffuse, 1);
+    }
+    )";
 
 // Fonction de callback pour ajuster la taille de la fenêtre
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
@@ -117,6 +164,58 @@ GLuint createShader(GLenum type, const char* source) {
     return shader;
 }
 
+const char* sphereFragmentShaderSource = R"(
+    #version 330 core
+    out vec4 FragColor;
+    
+    void main()
+    {
+        FragColor = vec4(1.0, 1.0, 0.0, 1.0); // Couleur jaune
+    }
+    )";
+    
+GLuint createSphereShaderProgram() {
+    const char* vertexShaderSource = R"(
+    #version 330 core
+    layout (location = 0) in vec3 aPos;
+    layout (location = 1) in vec3 aNormal;
+
+    uniform mat4 model;
+    uniform mat4 view;
+    uniform mat4 projection;
+
+    void main()
+    {
+        gl_Position = projection * view * model * vec4(aPos, 1.0);
+    }
+    )";
+
+    GLuint vertexShader = createShader(GL_VERTEX_SHADER, vertexShaderSource);
+    GLuint fragmentShader = createShader(GL_FRAGMENT_SHADER, sphereFragmentShaderSource);
+    
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    int success;
+    char infoLog[512];
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(shaderProgram, 512, nullptr, infoLog);
+        std::cerr << "Erreur de linkage du programme shader:\n"
+                    << infoLog << "\n";
+    }
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    return shaderProgram;
+}
+
+
+
+
 GLuint createShaderProgram() {
     GLuint vertexShader = createShader(GL_VERTEX_SHADER, vertexShaderSource);
     GLuint fragmentShader = createShader(GL_FRAGMENT_SHADER, fragmentShaderSource);
@@ -141,24 +240,341 @@ GLuint createShaderProgram() {
     return shaderProgram;
 }
 
+// Variables pour la position de la caméra
+glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+glm::vec3 lightPos = glm::vec3(-2.0f, 0.0f, 0.0f);
+bool fastSqrt = true;
+
+
+glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
+float cameraSpeed = 0.0005f; // ajustez la vitesse de la caméra
+
+// Variables pour la gestion de la souris
+float lastX = 400, lastY = 300;
+float yaw = -90.0f, pitch = 0.0f;
+bool firstMouse = true;
+float sensitivity = 0.1f;
+
+void processInput(GLFWwindow *window) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        cameraPos += cameraSpeed * cameraFront;
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        cameraPos -= cameraSpeed * cameraFront;
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+    if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS)
+        cameraPos += cameraSpeed * cameraUp;
+    if (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS)
+        cameraPos -= cameraSpeed * cameraUp;
+    if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
+        lightPos.y += cameraSpeed;
+    if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
+        lightPos.y -= cameraSpeed;
+    if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS)
+        lightPos.x -= cameraSpeed;
+    if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS)
+        lightPos.x += cameraSpeed;
+    if (glfwGetKey(window, GLFW_KEY_V) == GLFW_PRESS)
+        fastSqrt = !fastSqrt;
+}
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset) {
+    // change la puissnace de la lumiere
+    if (glfwGetKey(window, GLFW_KEY_LEFT_CONTROL) == GLFW_PRESS || glfwGetKey(window, GLFW_KEY_RIGHT_CONTROL) == GLFW_PRESS) {
+        cameraSpeed += yoffset * 0.0001f;
+        if (cameraSpeed < 0.0001f) cameraSpeed = 0.0001f; 
+    } else {
+    }
+}
+
+
+void mouse_callback(GLFWwindow* window, double xpos, double ypos) {
+    if (firstMouse) {
+        lastX = xpos;
+        lastY = ypos;
+        firstMouse = false;
+    }
+
+    float xoffset = xpos - lastX;
+    float yoffset = lastY - ypos; // inversé car les coordonnées y vont de bas en haut
+    lastX = xpos;
+    lastY = ypos;
+
+    xoffset *= sensitivity;
+    yoffset *= sensitivity;
+
+    yaw += xoffset;
+    pitch += yoffset;
+
+    // Assurez-vous que lorsque le pitch est hors de ces limites, lZ'écran ne se retourne pas
+    if (pitch > 89.0f)
+        pitch = 89.0f;
+    if (pitch < -89.0f)
+        pitch = -89.0f;
+
+    glm::vec3 front;
+    front.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+    front.y = sin(glm::radians(pitch));
+    front.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+    cameraFront = glm::normalize(front);
+}
+
 int main() {
     GLFWwindow* window = initOpenGL();
+
+    // Obtenir la taille de l'écran
+    const GLFWvidmode* mode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+    int screenWidth = mode->width;
+    int screenHeight = mode->height;
+
+    // Définir la taille de la fenêtre
+    int windowWidth = 2880;
+    int windowHeight = 1920;
+    glfwSetWindowSize(window, windowWidth, windowHeight);
+
+    // Calculer la position pour centrer la fenêtre
+    int windowPosX = (screenWidth - windowWidth) / 2;
+    int windowPosY = (screenHeight - windowHeight) / 2;
+    glfwSetWindowPos(window, windowPosX, windowPosY);
+
     if (!window) return -1;
 
-    GLuint shaderProgram = createShaderProgram();
+    // Définir le rappel de la souris et de la molette
+    glfwSetCursorPosCallback(window, mouse_callback);
+    glfwSetScrollCallback(window, scroll_callback);
 
-    // --- Code pour initialiser un cube et son rendu à ajouter ici ---
+    // Capturer le curseur
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+    GLuint shaderProgram = createShaderProgram();
+    GLuint sphereShaderProgram = createSphereShaderProgram();
+
+    // Initialisation des données du petit cube
+    float smallCubeVertices[] = {
+        // positions          // normales
+        // Face arrière
+        -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+         0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+         0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+         0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+    
+        // Face avant
+        -0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+         0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+         0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+         0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+    
+        // Face gauche
+        -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
+        -0.5f,  0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
+        -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
+        -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
+        -0.5f, -0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
+        -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
+    
+        // Face droite
+         0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
+         0.5f,  0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
+         0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
+         0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
+         0.5f, -0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
+         0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
+    
+        // Face inférieure
+        -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
+         0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
+         0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
+         0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
+    
+        // Face supérieure
+        -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,
+         0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,
+         0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
+         0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f
+    };
+
+    // Génération des données de la sphère
+    std::vector<float> sphereVertices;
+    std::vector<unsigned int> sphereIndices;
+    const unsigned int X_SEGMENTS = 64;
+    const unsigned int Y_SEGMENTS = 64;
+    const float PI = 3.14159265359f;
+
+    for (unsigned int y = 0; y <= Y_SEGMENTS; ++y) {
+        for (unsigned int x = 0; x <= X_SEGMENTS; ++x) {
+            float xSegment = (float)x / (float)X_SEGMENTS;
+            float ySegment = (float)y / (float)Y_SEGMENTS;
+            float xPos = std::cos(xSegment * 2.0f * PI) * std::sin(ySegment * PI);
+            float yPos = std::cos(ySegment * PI);
+            float zPos = std::sin(xSegment * 2.0f * PI) * std::sin(ySegment * PI);
+
+            sphereVertices.push_back(xPos);
+            sphereVertices.push_back(yPos);
+            sphereVertices.push_back(zPos);
+            sphereVertices.push_back(xPos);
+            sphereVertices.push_back(yPos);
+            sphereVertices.push_back(zPos);
+        }
+    }
+
+    bool oddRow = false;
+    for (unsigned int y = 0; y < Y_SEGMENTS; ++y) {
+        if (!oddRow) {
+            for (unsigned int x = 0; x <= X_SEGMENTS; ++x) {
+                sphereIndices.push_back(y * (X_SEGMENTS + 1) + x);
+                sphereIndices.push_back((y + 1) * (X_SEGMENTS + 1) + x);
+            }
+        } else {
+            for (int x = X_SEGMENTS; x >= 0; --x) {
+                sphereIndices.push_back((y + 1) * (X_SEGMENTS + 1) + x);
+                sphereIndices.push_back(y * (X_SEGMENTS + 1) + x);
+            }
+        }
+        oddRow = !oddRow;
+    }
+    GLuint sphereVAO, sphereVBO, sphereEBO;
+    glGenVertexArrays(1, &sphereVAO);
+    glGenBuffers(1, &sphereVBO);
+    glGenBuffers(1, &sphereEBO);
+
+    glBindVertexArray(sphereVAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, sphereVBO);
+    glBufferData(GL_ARRAY_BUFFER, sphereVertices.size() * sizeof(float), &sphereVertices[0], GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, sphereEBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sphereIndices.size() * sizeof(unsigned int), &sphereIndices[0], GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+// fin shpère
+
+
+    GLuint smallVBO, smallVAO;
+    glGenVertexArrays(1, &smallVAO);
+    glGenBuffers(1, &smallVBO);
+
+    glBindVertexArray(smallVAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, smallVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(smallCubeVertices), smallCubeVertices, GL_STATIC_DRAW);
+
+    // position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    // normal attribute
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    GLuint largeVBO, largeVAO;
+    glGenVertexArrays(1, &largeVAO);
+    glGenBuffers(1, &largeVBO);
+
+    glBindVertexArray(largeVAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, largeVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(smallCubeVertices), smallCubeVertices, GL_STATIC_DRAW);
+
+    // position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    // normal attribute
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    // Définir les positions de la lumière et de la caméra
+
+    glm::vec3 viewPos(0.0f, 0.0f, 3.0f);
 
     while (!glfwWindowShouldClose(window)) {
+        processInput(window);
+    
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
+    
+        // Utiliser le shader pour le petit cube (avec éclairage)
         glUseProgram(shaderProgram);
+    
+        // Mettre à jour la position de la lumière pour qu'elle suive la caméra
+        // lightPos = cameraPos;
+    
+        // Définir les matrices de transformation
+        glm::mat4 model = glm::mat4(1.0f);
+        glm::mat4 view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
+        glm::mat4 projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
+    
+        // Passer les matrices aux shaders cube
+        unsigned int modelLoc = glGetUniformLocation(shaderProgram, "model");
+        unsigned int viewLoc = glGetUniformLocation(shaderProgram, "view");
+        unsigned int projectionLoc = glGetUniformLocation(shaderProgram, "projection");
+        unsigned int fastSqrtLoc = glGetUniformLocation(shaderProgram, "fastSqrt");
 
-        // --- Code pour définir matrices de transformation et dessiner le cube ---
+        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+        glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
+        
+        // Passer les positions de la lumière et de la caméra aux shaders
+        glUniform3fv(glGetUniformLocation(shaderProgram, "lightPos"), 1, &lightPos[0]);
+        glUniform3fv(glGetUniformLocation(shaderProgram, "viewPos"), 1, &viewPos[0]);
+        glUniform1i(fastSqrtLoc, fastSqrt);
 
+        
+        // Dessiner le petit cube
+        glBindVertexArray(smallVAO);
+        glm::mat4 smallCubeModel = glm::mat4(1.0f);
+        smallCubeModel = glm::translate(smallCubeModel, glm::vec3(0.0f, 0.0f, 0.0f)); // Positionner le cube
+        smallCubeModel = glm::scale(smallCubeModel, glm::vec3(2.0f, 2.0f, 2.0f));
+        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(smallCubeModel));
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+        
+
+        // Dessiner la pièce (grand cube)
+        glBindVertexArray(largeVAO);
+        glm::mat4 largeCubeModel = glm::mat4(1.0f);
+        largeCubeModel = glm::translate(largeCubeModel, glm::vec3(0.0f, 0.0f, 0.0f)); // Positionner le grand cube
+        largeCubeModel = glm::scale(largeCubeModel, glm::vec3(10.0f, 10.0f, 10.0f)); // Définir la taille du grand cube
+        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(largeCubeModel));
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+
+        // sphère
+        glUseProgram(sphereShaderProgram);
+
+        modelLoc = glGetUniformLocation(sphereShaderProgram, "model");
+        viewLoc = glGetUniformLocation(sphereShaderProgram, "view");
+        projectionLoc = glGetUniformLocation(sphereShaderProgram, "projection");
+        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+        glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
+    
+        glBindVertexArray(sphereVAO);
+        glm::mat4 sphereModel = glm::mat4(1.0f);
+        sphereModel = glm::translate(sphereModel, glm::vec3(lightPos)); // Positionner la sphère
+        sphereModel = glm::scale(sphereModel, glm::vec3(0.1f, 0.1f, 0.1f));
+        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(sphereModel));
+        glDrawElements(GL_TRIANGLE_STRIP, sphereIndices.size(), GL_UNSIGNED_INT, 0);
+
+    
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
+
+    glDeleteVertexArrays(1, &smallVAO);
+    glDeleteBuffers(1, &smallVBO);
+    glDeleteVertexArrays(1, &largeVAO);
+    glDeleteBuffers(1, &largeVBO);
 
     glfwTerminate();
     return 0;
