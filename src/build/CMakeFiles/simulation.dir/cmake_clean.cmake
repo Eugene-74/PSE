98779b9a13a25f91e21b6ce10b803d20a712eb82
Eugene-74 <eugenezrg@gmail.com>
2025-05-20 15:17:54 +0200

    push a release

---
+++ src/build/CMakeFiles/simulation.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/simulation.dir/glad.c.obj"
+  "CMakeFiles/simulation.dir/glad.c.obj.d"
+  "CMakeFiles/simulation.dir/simulation.cpp.obj"
+  "CMakeFiles/simulation.dir/simulation.cpp.obj.d"
+  "libsimulation.dll.a"
+  "simulation.exe"
+  "simulation.exe.manifest"
+  "simulation.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/simulation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
